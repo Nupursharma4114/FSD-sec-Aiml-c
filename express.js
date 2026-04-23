5579989e3271efe3c047a3fe590bdb38c49354bb
Nupursharma4114 <nupursharma4114@gmail.com>
2026-04-23 15:00:09 +0530

    Update comment for clarity

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 9000;
 // Commit 1: Initial comment
-// Commit 1: Nupur Sharma
+// Commit 1: Nupur Sharma from AMU
 // Middleware for parsing JSON
 app.use(express.json());
 
